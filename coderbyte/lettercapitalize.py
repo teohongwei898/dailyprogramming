7382ea0b59104f2e1ffff44e6c5307eb72b3d09a
Hong Wei <teohongwei898@gmail.com>
2019-02-12 11:10:39 +0800

    Create lettercapitalize.py

---
+++ coderbyte/lettercapitalize.py
@@ -0,0 +1,7 @@
+def LetterCapitalize(str): 
+
+  # in python there is a function called title which is
+  # easier than using a regex pattern
+  return str.title()
+    
+print LetterCapitalize("hello world from coderbyte")     
